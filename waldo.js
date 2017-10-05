a10fb65ce1dc18d76219c22fa9ab237dc4d5ae2f
Ryan <ryaninbrazil@gmail.com>
2017-10-05 19:13:32 +0000

    Found Waldo, included Index

---
+++ waldo.js
@@ -0,0 +1,17 @@
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      found(i);   // execute callback
+    }
+  }
+}
+
+function actionWhenFound(position) {
+  
+  console.log("Found him!" + " " + position);
+  
+}
+
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
